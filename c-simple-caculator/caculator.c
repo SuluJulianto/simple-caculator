d8e92f25fde30d091aa2931ed64bfe98998f51f5
Sulu Edward Julianto <93524287+SuluJulianto@users.noreply.github.com>
2024-01-05 16:53:46 +0700

    Update Config and Code

--- c-simple-caculator/caculator.c
+++ c-simple-caculator/caculator.c
@@ -1,63 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int main() {
     char calculate[50];
     printf("Enter a math operation: ");
     fgets(calculate, sizeof(calculate), stdin);
-    
+
+    // Remove newline character if present in the input
     if (calculate[strlen(calculate) - 1] == '\n') {
         calculate[strlen(calculate) - 1] = '\0';
     }
 
-    int num1, num2;
+    int num1, num2, answer;
     char operator;
 
-    char *operators = "+-*/%^";
-    char *op = strpbrk(calculate, operators);
-
-    if (op != NULL) {
-        operator = *op;
-
-        sscanf(calculate, "%d %c %d", &num1, &operator, &num2);
-
-        int result;
+    if (sscanf(calculate, "%d %c %d", &num1, &operator, &num2) == 3) {
         switch (operator) {
             case '+':
-                result = num1 + num2;
+                answer = num1 + num2;
+                printf("%d + %d = %d\nYour answer is %d\n", num1, num2, answer, answer);
                 break;
             case '-':
-                result = num1 - num2;
+                answer = num1 - num2;
+                printf("%d - %d = %d\nYour answer is %d\n", num1, num2, answer, answer);
                 break;
             case '*':
-                result = num1 * num2;
+                answer = num1 * num2;
+                printf("%d * %d = %d\nYour answer is %d\n", num1, num2, answer, answer);
                 break;
             case '/':
                 if (num2 != 0) {
-                    result = num1 / num2;
+                    float result = (float)num1 / num2;
+                    printf("%d / %d = %.2f\nYour answer is %.2f\n", num1, num2, result, result);
                 } else {
-                    printf("Division by zero is not allowed.\n");
-                    return 1;
+                    printf("Error! Division by zero is not allowed.\n");
                 }
                 break;
             case '%':
-                result = num1 % num2;
-                break;
-            case '^':
-                result = 1;
-                for (int i = 0; i < num2; ++i) {
-                    result *= num1;
-                }
+                answer = num1 % num2;
+                printf("%d %% %d = %d\nYour answer is %d\n", num1, num2, answer, answer);
                 break;
             default:
-                printf("Operator is not recognized.\n");
-                return 1;
+                printf("Operation not recognized or not supported.\n");
         }
-
-        printf("The result of %d %c %d is: %d\n", num1, operator, num2, result);
     } else {
-        printf("No operator found or invalid format.\n");
+        printf("Invalid input format.\n");
     }
 
     return 0;
